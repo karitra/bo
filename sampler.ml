0357e794c9db99015dfdc80106b8ec309ff34984
karitra <karev.alexandre@gmail.com>
2016-08-23 02:04:47 +0300

    remove float parsing redundency

--- sampler.ml
+++ sampler.ml
@@ -7,30 +7,20 @@ module Sampler = struct
 	
 	let make fname k =
 
-		let samples = Array.create ~len:k [] in
-		let make_vec_list ln =
-			String.split ~on:',' ln
-			|> List.map
-				~f:(fun tok -> 
-					if String.length tok = 0 then 
-						Float.nan 
-					else 
-						Float.of_string tok)
-		in
+		let samples = Array.create ~len:k "" in
 
 		let f ic =
 			let f ln =
-				let li = make_vec_list ln in
 				let j = Random.int k in
 					if j < k then
-						samples.(j) <- li
+						samples.(j) <- ln
 					else ()
 			in
 			
 			let header = Option.value ~default:"" (In_channel.input_line ic) in
 			
 			for i = 0 to k - 1 do match In_channel.input_line ic with
-				| Some ln -> samples.(i) <- make_vec_list ln
+				| Some ln -> samples.(i) <- ln
 				| None -> ()
 			done;
 			In_channel.iter_lines ic ~f;
@@ -40,12 +30,18 @@ module Sampler = struct
 		In_channel.with_file fname ~f
 
 	let dump ?(delim=',') header samples =
-		fprintf stdout "%s\n" header;
-		let rec dump_row = function
-			| hd::tl -> (printf "%f%c" hd delim; dump_row tl)
-			| [] -> print_endline ""
-		in
-		Array.iter ~f:(fun l -> dump_row l) samples
+		printf "%s\n" header;
+		Array.sort
+			~cmp:(fun a b -> 
+				let r1 = String.index a delim and
+					r2 = String.index b delim in
+					match Option.both r1 r2 with
+					| Some (r1,r2) -> 
+						let a = String.prefix a r1 |> Int.of_string and
+							b = String.prefix b r2 |> Int.of_string in
+								a - b
+					| None -> 0) samples;
+		Array.iter ~f:print_endline samples
 
 end
 
@@ -55,10 +51,10 @@ let () =
 			Sampler.dump header samples
 	in
 	Command.basic
-		~summary:"sample K records from bosch kaggle.com dataset"
+		~summary:"sample K records from bosch kaggle's dataset"
 		Command.Spec.(
 			empty
-			+> flag "-input" (required string) ~doc:"filename CSV file name to smaple from"
-			+> flag "-samples" (required int) ~doc:"K number of sampels" )
+			+> flag "-input" (required string) ~doc:"filename CSV file name to sample from"
+			+> flag "-samples" (required int) ~doc:"K number of samples" )
 		cmd
 	|> Command.run ~version:"1.0" 
